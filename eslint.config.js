5cae187f4276592149cc7abd10ce712c9e4fdbe8
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2025-07-10 10:29:20 +0200

    build: add new eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+        // ...globals.commonjs,
+        // ...globals.node,
+      }
+      // ecmaVersion: 12,
+      // sourceType: "commonjs",
+    }
+  },
+
+  pluginJs.configs.recommended,
+
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn'
+    }
+  }
+]
