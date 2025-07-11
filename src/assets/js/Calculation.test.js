16e08889f2d8d0a14a1aa1c1ec05059540327121
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2025-07-11 14:36:59 +0200

    test(js): add better test cases and test data

--- src/assets/js/Calculation.test.js
+++ src/assets/js/Calculation.test.js
@@ -3,109 +3,239 @@ import { describe, beforeEach, test, expect } from '@jest/globals'
 import { Calculation } from './Calculation.js'
 
 describe('class Calculation', () => {
-  let calcExample1
-  let calcExample2
-  const gradeRating1 = 1300
-  const gradeRating2 = 2000
-  const gradePresentation = 2000
-  const gradeDiscussion = 1700
-  const variant1 = 1
-  const factorWrittenThesis1 = 2
-  const factorDefense1 = 1
-  const variant2 = 2
-  const factorWrittenThesis2 = 3
-  const factorDefense2 = 1
-  const gradeWrittenThesis = 1650
-  const gradeDefense = 1850
-  const gradeThesisVariant1 = 1666
-  const gradeThesisAsText1 = 'Gut'
-  const gradeThesisVariant2 = 1650
-  const gradeThesisAsText2 = 'Gut'
+  // Test data variant 1
+  const v1 = {
+    example: null,
+    input: {
+      gradeRating1: 1.3,
+      gradeRating2: 2,
+      gradePresentation: 2,
+      gradeDiscussion: 1.7,
+      variant: 1
+    },
+    output: {
+      gradeRating1: 1300,
+      gradeRating2: 2000,
+      gradePresentation: 2000,
+      gradeDiscussion: 1700,
+      variant: 1,
+      factorWrittenThesis: 2,
+      factorDefense: 1,
+      gradeWrittenThesis: 1650,
+      gradeDefense: 1850,
+      gradeThesis: 1666,
+      gradeThesisAsText: 'Gut'
+    }
+  }
+
+  // Test data variant 2
+  const v2 = {
+    example: null,
+    input: {
+      gradeRating1: 1.3,
+      gradeRating2: 2,
+      gradePresentation: 2,
+      gradeDiscussion: 1.7,
+      variant: 2
+    },
+    output: {
+      gradeRating1: 1300,
+      gradeRating2: 2000,
+      gradePresentation: 2000,
+      gradeDiscussion: 1700,
+      variant: 2,
+      factorWrittenThesis: 3,
+      factorDefense: 1,
+      gradeWrittenThesis: 1650,
+      gradeDefense: 1850,
+      gradeThesis: 1650,
+      gradeThesisAsText: 'Gut'
+    }
+  }
+
+  // Test data variant 3
+  const v3 = {
+    example: null,
+    input: {
+      gradeRating1: 1.3,
+      gradeRating2: 1.7,
+      gradePresentation: 1.7,
+      gradeDiscussion: 1.7,
+      variant: 1
+    },
+    output: {
+      gradeRating1: 1300,
+      gradeRating2: 1700,
+      gradePresentation: 1700,
+      gradeDiscussion: 1700,
+      variant: 1,
+      factorWrittenThesis: 2,
+      factorDefense: 1,
+      gradeWrittenThesis: 1500,
+      gradeDefense: 1700,
+      gradeThesis: 1566,
+      gradeThesisAsText: 'Sehr gut'
+    }
+  }
 
   beforeEach(() => {
-    calcExample1 = new Calculation()
-    calcExample1.setInputGrades(1.3, 2, 2, 1.7, 1)
-    calcExample2 = new Calculation()
-    calcExample2.setInputGrades(1.3, 2, 2, 1.7, 2)
+    v1.example = new Calculation()
+    v1.example.setInputGrades(
+      v1.input.gradeRating1,
+      v1.input.gradeRating2,
+      v1.input.gradePresentation,
+      v1.input.gradeDiscussion,
+      v1.input.variant
+    )
+    v2.example = new Calculation()
+    v2.example.setInputGrades(
+      v2.input.gradeRating1,
+      v2.input.gradeRating2,
+      v2.input.gradePresentation,
+      v2.input.gradeDiscussion,
+      v2.input.variant
+    )
+    v3.example = new Calculation()
+    v3.example.setInputGrades(
+      v3.input.gradeRating1,
+      v3.input.gradeRating2,
+      v3.input.gradePresentation,
+      v3.input.gradeDiscussion,
+      v3.input.variant
+    )
   })
 
-  test('Constructor / set / get', () => {
-    const calc = new Calculation()
-    expect(calc.gradeRating1.value).toBe(0)
-    expect(calc.gradeRating2.value).toBe(0)
-    expect(calc.gradePresentation.value).toBe(0)
-    expect(calc.gradeDiscussion.value).toBe(0)
-    expect(calc.variant).toBe(0)
-    expect(calc.gradeWrittenThesis.value).toBe(0)
-    expect(calc.gradeDefense.value).toBe(0)
-    expect(calc.gradeThesis.value).toBe(0)
-    expect(calc.gradeThesisAsText).toBe('')
+  test('Constructor (no arguments)', () => {
+    const calculation = new Calculation()
+    expect(calculation.gradeRating1.value).toBe(0)
+    expect(calculation.gradeRating2.value).toBe(0)
+    expect(calculation.gradePresentation.value).toBe(0)
+    expect(calculation.gradeDiscussion.value).toBe(0)
+    expect(calculation.variant).toBe(0)
+    expect(calculation.gradeWrittenThesis.value).toBe(0)
+    expect(calculation.gradeDefense.value).toBe(0)
+    expect(calculation.gradeThesis.value).toBe(0)
+    expect(calculation.gradeThesisAsText).toBe('')
   })
 
   test('variant set / get', () => {
-    calcExample1.variant = variant1
-    calcExample2.variant = variant2
-    expect(calcExample1.variant).toBe(variant1)
-    expect(calcExample2.variant).toBe(variant2)
-    expect(calcExample1.factorWrittenThesis).toBe(factorWrittenThesis1)
-    expect(calcExample2.factorWrittenThesis).toBe(factorWrittenThesis2)
-    expect(calcExample1.factorDefense).toBe(factorDefense1)
-    expect(calcExample2.factorDefense).toBe(factorDefense2)
-    expect(calcExample1.factorDivisor).toBe(factorWrittenThesis1 + factorDefense1)
-    expect(calcExample2.factorDivisor).toBe(factorWrittenThesis2 + factorDefense2)
+    // variant 1
+    v1.example.variant = v1.input.variant
+    expect(v1.example.variant).toBe(v1.output.variant)
+    expect(v1.example.factorWrittenThesis).toBe(v1.output.factorWrittenThesis)
+    expect(v1.example.factorDefense).toBe(v1.output.factorDefense)
+    expect(v1.example.factorDivisor).toBe(v1.output.factorWrittenThesis + v1.output.factorDefense)
+
+    // variant 2
+    v2.example.variant = v2.input.variant
+    expect(v2.example.variant).toBe(v2.output.variant)
+    expect(v2.example.factorWrittenThesis).toBe(v2.output.factorWrittenThesis)
+    expect(v2.example.factorDefense).toBe(v2.output.factorDefense)
+    expect(v2.example.factorDivisor).toBe(v2.output.factorWrittenThesis + v2.output.factorDefense)
+
+    // variant 3
+    v3.example.variant = v3.input.variant
+    expect(v3.example.variant).toBe(v3.output.variant)
+    expect(v3.example.factorWrittenThesis).toBe(v3.output.factorWrittenThesis)
+    expect(v3.example.factorDefense).toBe(v3.output.factorDefense)
+    expect(v3.example.factorDivisor).toBe(v3.output.factorWrittenThesis + v3.output.factorDefense)
   })
 
   test('setInputGrades()', () => {
-    expect(calcExample1.gradeRating1.value).toBe(gradeRating1)
-    expect(calcExample1.gradeRating2.value).toBe(gradeRating2)
-    expect(calcExample1.gradePresentation.value).toBe(gradePresentation)
-    expect(calcExample1.gradeDiscussion.value).toBe(gradeDiscussion)
-    expect(calcExample1.variant).toBe(variant1)
-  })
+    // variant 1
+    expect(v1.example.gradeRating1.value).toBe(v1.output.gradeRating1)
+    expect(v1.example.gradeRating2.value).toBe(v1.output.gradeRating2)
+    expect(v1.example.gradePresentation.value).toBe(v1.output.gradePresentation)
+    expect(v1.example.gradeDiscussion.value).toBe(v1.output.gradeDiscussion)
+    expect(v1.example.variant).toBe(v1.output.variant)
 
-  test('calc()', () => {
-    // calc variant 1
-    calcExample1.calc()
-    expect(calcExample1.variant).toBe(variant1)
-    expect(calcExample1.gradeWrittenThesis.value).toBe(gradeWrittenThesis)
-    expect(calcExample1.gradeDefense.value).toBe(gradeDefense)
-    expect(calcExample1.gradeThesis.value).toBe(gradeThesisVariant1)
-    expect(calcExample1.gradeThesisAsText).toBe(gradeThesisAsText1)
+    // variant 2
+    expect(v2.example.gradeRating1.value).toBe(v2.output.gradeRating1)
+    expect(v2.example.gradeRating2.value).toBe(v2.output.gradeRating2)
+    expect(v2.example.gradePresentation.value).toBe(v2.output.gradePresentation)
+    expect(v2.example.gradeDiscussion.value).toBe(v2.output.gradeDiscussion)
+    expect(v2.example.variant).toBe(v2.output.variant)
 
-    // calc variant 2
-    calcExample2.calc()
-    expect(calcExample2.variant).toBe(variant2)
-    expect(calcExample2.gradeWrittenThesis.value).toBe(gradeWrittenThesis)
-    expect(calcExample2.gradeDefense.value).toBe(gradeDefense)
-    expect(calcExample2.gradeThesis.value).toBe(gradeThesisVariant2)
-    expect(calcExample2.gradeThesisAsText).toBe(gradeThesisAsText2)
+    // variant 3
+    expect(v3.example.gradeRating1.value).toBe(v3.output.gradeRating1)
+    expect(v3.example.gradeRating2.value).toBe(v3.output.gradeRating2)
+    expect(v3.example.gradePresentation.value).toBe(v3.output.gradePresentation)
+    expect(v3.example.gradeDiscussion.value).toBe(v3.output.gradeDiscussion)
+    expect(v3.example.variant).toBe(v3.output.variant)
   })
 
   test('calcGradeWrittenThesis()', () => {
-    calcExample1.calcGradeWrittenThesis()
-    expect(calcExample1.gradeWrittenThesis.value).toBe(gradeWrittenThesis)
+    // variant 1
+    v1.example.calcGradeWrittenThesis()
+    expect(v1.example.gradeWrittenThesis.value).toBe(v1.output.gradeWrittenThesis)
+
+    // variant 2
+    v2.example.calcGradeWrittenThesis()
+    expect(v2.example.gradeWrittenThesis.value).toBe(v2.output.gradeWrittenThesis)
+
+    // variant 3
+    v3.example.calcGradeWrittenThesis()
+    expect(v3.example.gradeWrittenThesis.value).toBe(v3.output.gradeWrittenThesis)
   })
 
   test('calcGradeDefense()', () => {
-    const calc = calcExample1.clone()
-    calc.calcGradeDefense()
-    expect(calc.gradeDefense.value).toBe(gradeDefense)
+    // variant 1
+    v1.example.calcGradeDefense()
+    expect(v1.example.gradeDefense.value).toBe(v1.output.gradeDefense)
+
+    // variant 2
+    v2.example.calcGradeDefense()
+    expect(v2.example.gradeDefense.value).toBe(v2.output.gradeDefense)
+
+    // variant 3
+    v3.example.calcGradeDefense()
+    expect(v3.example.gradeDefense.value).toBe(v3.output.gradeDefense)
   })
 
   test('calcGradeThesis()', () => {
-    // calc variant 1
-    const calc1 = calcExample1.clone()
-    calc1.calcGradeThesis()
-    expect(calcExample1.variant).toBe(variant1)
-    expect(calc1.gradeThesis.value).toBe(gradeThesisVariant1)
-    expect(calcExample1.gradeThesisAsText).toBe(gradeThesisAsText1)
+    // variant 1
+    v1.example.calcGradeThesis()
+    expect(v1.example.variant).toBe(v1.output.variant)
+    expect(v1.example.gradeThesis.value).toBe(v1.output.gradeThesis)
+    expect(v1.example.gradeThesisAsText).toBe(v1.output.gradeThesisAsText)
 
-    // calc variant 2
-    const calc2 = calcExample2.clone()
-    calc2.calcGradeThesis()
-    expect(calcExample2.variant).toBe(variant2)
-    expect(calc2.gradeThesis.value).toBe(gradeThesisVariant2)
-    expect(calcExample2.gradeThesisAsText).toBe(gradeThesisAsText2)
+    // variant 2
+    v2.example.calcGradeThesis()
+    expect(v2.example.variant).toBe(v2.output.variant)
+    expect(v2.example.gradeThesis.value).toBe(v2.output.gradeThesis)
+    expect(v2.example.gradeThesisAsText).toBe(v2.output.gradeThesisAsText)
+
+    // variant 3
+    v3.example.calcGradeThesis()
+    expect(v3.example.variant).toBe(v3.output.variant)
+    expect(v3.example.gradeThesis.value).toBe(v3.output.gradeThesis)
+    expect(v3.example.gradeThesisAsText).toBe(v3.output.gradeThesisAsText)
+  })
+
+  test('calc()', () => {
+    // variant 1
+    v1.example.calc()
+    expect(v1.example.variant).toBe(v1.output.variant)
+    expect(v1.example.gradeWrittenThesis.value).toBe(v1.output.gradeWrittenThesis)
+    expect(v1.example.gradeDefense.value).toBe(v1.output.gradeDefense)
+    expect(v1.example.gradeThesis.value).toBe(v1.output.gradeThesis)
+    expect(v1.example.gradeThesisAsText).toBe(v1.output.gradeThesisAsText)
+
+    // variant 2
+    v2.example.calc()
+    expect(v2.example.variant).toBe(v2.output.variant)
+    expect(v2.example.gradeWrittenThesis.value).toBe(v2.output.gradeWrittenThesis)
+    expect(v2.example.gradeDefense.value).toBe(v2.output.gradeDefense)
+    expect(v2.example.gradeThesis.value).toBe(v2.output.gradeThesis)
+    expect(v2.example.gradeThesisAsText).toBe(v2.output.gradeThesisAsText)
+
+    // variant 3
+    v3.example.calc()
+    expect(v3.example.variant).toBe(v3.output.variant)
+    expect(v3.example.gradeWrittenThesis.value).toBe(v3.output.gradeWrittenThesis)
+    expect(v3.example.gradeDefense.value).toBe(v3.output.gradeDefense)
+    expect(v3.example.gradeThesis.value).toBe(v3.output.gradeThesis)
+    expect(v3.example.gradeThesisAsText).toBe(v3.output.gradeThesisAsText)
   })
 
   test('getGradeAsText()', () => {
@@ -116,5 +246,19 @@ describe('class Calculation', () => {
     expect(calc.getGradeAsText(3500)).toBe('Befriedigend')
     expect(calc.getGradeAsText(4000)).toBe('Ausreichend')
     expect(calc.getGradeAsText(4001)).toBe('Nicht ausreichend')
+  })
+
+  test('clone()', () => {
+    // variant 1
+    const clone1 = v1.example.clone()
+    expect(clone1.variant).toBe(v1.output.variant)
+    expect(clone1.gradeRating1.value).toBe(v1.output.gradeRating1)
+    expect(clone1.gradeRating2.value).toBe(v1.output.gradeRating2)
+    expect(clone1.gradePresentation.value).toBe(v1.output.gradePresentation)
+    expect(clone1.gradeDiscussion.value).toBe(v1.output.gradeDiscussion)
+    expect(clone1.gradeWrittenThesis.value).toBe(v1.output.gradeWrittenThesis)
+    expect(clone1.gradeDefense.value).toBe(v1.output.gradeDefense)
+    expect(clone1.gradeThesis.value).toBe(v1.output.gradeThesis)
+    expect(clone1.gradeThesisAsText).toBe(v1.output.gradeThesisAsText)
   })
 })
--- src/assets/js/Grade.test.js
+++ src/assets/js/Grade.test.js
@@ -3,55 +3,156 @@ import { describe, test, expect } from '@jest/globals'
 import { Grade } from './Grade.js'
 
 describe('class Grade', () => {
-  test('Constructor / set / get', () => {
-    // Constructor / get
-    let grade = new Grade(2300)
-    expect(grade.value).toBe(2300)
-    grade = new Grade()
+  test('Constructor / set', () => {
+    let grade = new Grade()
     expect(grade.value).toBe(0)
     grade = new Grade(999)
     expect(grade.value).toBe(0)
+    grade = new Grade(999.99)
+    expect(grade.value).toBe(0)
+    grade = new Grade(1000)
+    expect(grade.value).toBe(1000)
+    grade = new Grade(1000.99)
+    expect(grade.value).toBe(1000)
+    grade = new Grade(5000)
+    expect(grade.value).toBe(5000)
     grade = new Grade(5001)
     expect(grade.value).toBe(0)
+  })
 
-    // set / get
-    grade.value = 2300
-    expect(grade.value).toBe(2300)
+  test('value set / get', () => {
+    let grade = new Grade()
     grade.value = 999
     expect(grade.value).toBe(0)
+    grade.value = 999.99
+    expect(grade.value).toBe(0)
+    grade.value = 1000
+    expect(grade.value).toBe(1000)
+    grade.value = 1000.99
+    expect(grade.value).toBe(1000)
+    grade.value = 5000
+    expect(grade.value).toBe(5000)
     grade.value = 5001
     expect(grade.value).toBe(0)
   })
 
-  test('valueShort()', () => {
-    let grade = new Grade(2299)
-    expect(grade.valueShort).toBe(2200)
-    grade = new Grade(2300)
-    expect(grade.valueShort).toBe(2300)
-    grade = new Grade(2399)
-    expect(grade.valueShort).toBe(2300)
+  test('valueShort get', () => {
+    let grade = new Grade()
+    expect(grade.valueShort).toBe(0)
+    grade.value = 999
+    expect(grade.valueShort).toBe(0)
+    grade.value = 1000
+    expect(grade.valueShort).toBe(1000)
+    grade.value = 1001
+    expect(grade.valueShort).toBe(1000)
+    grade.value = 1099
+    expect(grade.valueShort).toBe(1000)
+    grade.value = 1100
+    expect(grade.valueShort).toBe(1100)
+    grade.value = 4999
+    expect(grade.valueShort).toBe(4900)
+    grade.value = 5000
+    expect(grade.valueShort).toBe(5000)
+    grade.value = 5001
+    expect(grade.valueShort).toBe(0)
   })
 
-  test('valueAsString()', () => {
-    const grade = new Grade(2345)
-    expect(grade.valueAsString).toBe('2,345')
+  test('valueAsString get', () => {
+    const grade = new Grade()
+    expect(grade.valueAsString).toBe('0,000')
+    grade.value = 999
+    expect(grade.valueAsString).toBe('0,000')
+    grade.value = 1000
+    expect(grade.valueAsString).toBe('1,000')
+    grade.value = 1001
+    expect(grade.valueAsString).toBe('1,001')
+    grade.value = 1099
+    expect(grade.valueAsString).toBe('1,099')
+    grade.value = 1100
+    expect(grade.valueAsString).toBe('1,100')
+    grade.value = 4999
+    expect(grade.valueAsString).toBe('4,999')
+    grade.value = 5000
+    expect(grade.valueAsString).toBe('5,000')
+    grade.value = 5001
+    expect(grade.valueAsString).toBe('0,000')
   })
 
-  test('valueShortAsString()', () => {
-    const grade = new Grade(2345)
-    expect(grade.valueShortAsString).toBe('2,3')
+  test('valueShortAsString get', () => {
+    const grade = new Grade()
+    expect(grade.valueShortAsString).toBe('0,0')
+    grade.value = 999
+    expect(grade.valueShortAsString).toBe('0,0')
+    grade.value = 1000
+    expect(grade.valueShortAsString).toBe('1,0')
+    grade.value = 1001
+    expect(grade.valueShortAsString).toBe('1,0')
+    grade.value = 1099
+    expect(grade.valueShortAsString).toBe('1,0')
+    grade.value = 1100
+    expect(grade.valueShortAsString).toBe('1,1')
+    grade.value = 4999
+    expect(grade.valueShortAsString).toBe('4,9')
+    grade.value = 5000
+    expect(grade.valueShortAsString).toBe('5,0')
+    grade.value = 5001
+    expect(grade.valueShortAsString).toBe('0,0')
+  })
+
+  test('convertToShort()', () => {
+    const grade = new Grade()
+    expect(grade.convertToShort(0)).toBe(0)
+    expect(grade.convertToShort(9)).toBe(0)
+    expect(grade.convertToShort(99)).toBe(0)
+    expect(grade.convertToShort(999)).toBe(900)
+    expect(grade.convertToShort(9999)).toBe(9900)
+    expect(grade.convertToShort(99999)).toBe(99900)
   })
 
   test('convertToString()', () => {
-    const grade = new Grade(2345)
-    expect(grade.convertToString(2345)).toBe('2,3')
-    expect(grade.convertToString(2345, true)).toBe('2,3')
-    expect(grade.convertToString(2345, false)).toBe('2,345')
+    const grade = new Grade()
+    expect(grade.convertToString(0)).toBe('0,0')
+    expect(grade.convertToString(0, true)).toBe('0,0')
+    expect(grade.convertToString(0, false)).toBe('0,000')
+    expect(grade.convertToString(99)).toBe('0,0')
+    expect(grade.convertToString(99, true)).toBe('0,0')
+    expect(grade.convertToString(99, false)).toBe('0,099')
+    expect(grade.convertToString(999)).toBe('0,9')
+    expect(grade.convertToString(999, true)).toBe('0,9')
+    expect(grade.convertToString(999, false)).toBe('0,999')
+    expect(grade.convertToString(1000)).toBe('1,0')
+    expect(grade.convertToString(1000, true)).toBe('1,0')
+    expect(grade.convertToString(1000, false)).toBe('1,000')
+    expect(grade.convertToString(1001)).toBe('1,0')
+    expect(grade.convertToString(1001, true)).toBe('1,0')
+    expect(grade.convertToString(1001, false)).toBe('1,001')
+    expect(grade.convertToString(1099)).toBe('1,0')
+    expect(grade.convertToString(1099, true)).toBe('1,0')
+    expect(grade.convertToString(1099, false)).toBe('1,099')
+    expect(grade.convertToString(1100)).toBe('1,1')
+    expect(grade.convertToString(1100, true)).toBe('1,1')
+    expect(grade.convertToString(1100, false)).toBe('1,100')
+    expect(grade.convertToString(4999)).toBe('4,9')
+    expect(grade.convertToString(4999, true)).toBe('4,9')
+    expect(grade.convertToString(4999, false)).toBe('4,999')
+    expect(grade.convertToString(5000)).toBe('5,0')
+    expect(grade.convertToString(5000, true)).toBe('5,0')
+    expect(grade.convertToString(5000, false)).toBe('5,000')
+    expect(grade.convertToString(5001)).toBe('5,0')
+    expect(grade.convertToString(5001, true)).toBe('5,0')
+    expect(grade.convertToString(5001, false)).toBe('5,001')
+    expect(grade.convertToString(99999)).toBe('99,9')
+    expect(grade.convertToString(99999, true)).toBe('99,9')
+    expect(grade.convertToString(99999, false)).toBe('99,999')
   })
 
   test('floatToInt()', () => {
-    expect(Grade.floatToInt(2.345)).toBe(2345)
-    expect(Grade.floatToInt(2.3)).toBe(2300)
-    expect(Grade.floatToInt(2.345678)).toBe(2345)
+    expect(Grade.floatToInt(0)).toBe(0)
+    expect(Grade.floatToInt(1)).toBe(1000)
+    expect(Grade.floatToInt(1.2)).toBe(1200)
+    expect(Grade.floatToInt(1.23)).toBe(1230)
+    expect(Grade.floatToInt(1.234)).toBe(1234)
+    expect(Grade.floatToInt(1.2345)).toBe(1234)
+    expect(Grade.floatToInt(1.23456)).toBe(1234)
   })
 })
