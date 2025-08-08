654d130fd70c206b939776378e3d463e6d54fdd6
Christoph Zirkelbach <christoph.zirkelbach@htw-dresden.de>
2025-08-08 11:11:10 +0200

    ci(eslint): update config

--- eslint.config.js
+++ eslint.config.js
@@ -6,27 +6,48 @@ import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
+  // Sets the configuration name.
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,vue}']
   },
 
+  // Ignores specific Vue.js project folders.
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
+  // Uses the browser globals for default files (`{js,cjs,mjs}`).
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
-    },
+        ...globals.browser
+      }
+    }
   },
 
+  // Adds support/rules for JavaScript.
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  
+
+  // Adds support/rules for Vue.js.
+  ...pluginVue.configs['flat/recommended'],
+
+  // Adds support/rules for Vitest.
+  // - in all files in the __tests__ directory under src
+  // - in all files ending with .spec.js or .test.js under src
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    files: ['src/**/__tests__/*', 'src/**/*.{spec,test}.*']
   },
+
+  // Use ESLint only for linting and Prettier for formatting.
   skipFormatting,
+
+  // Overrides for specific rules.
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn'
+      // 'vue/multi-word-component-names': 'off',
+      // 'vue/no-reserved-component-names': 'off'
+    }
+  }
 ])
--- eslint.config_v1.js
+++ eslint.config_v1.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'eslint/config'
-import js from '@eslint/js'
 import globals from 'globals'
+import js from '@eslint/js'
 import eslintPluginVue from 'eslint-plugin-vue'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
